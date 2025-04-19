1b3ef0b70ad6ae58528302b9d2ec95bab49a4308
Parth Sharma <parthsharma@Parths-MacBook-Air.local>
2025-04-19 23:47:23 +0530

    Update deployment configuration

--- main.py
+++ main.py
@@ -377,4 +377,4 @@ def subscribe():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5001)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
